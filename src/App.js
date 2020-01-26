24cfd04722ee6118dd8606d3319a19c399e1f740
Will Powers <willpowers@Wills-MBP.fios-router.home>
2020-01-26 14:47:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          .... I did.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React
         </a>
       </header>
     </div>
